a20c5bc8afb1f2ccce7887e0aac73ae6b69a5add
Sara Hrg <hrgsara6@gmail.com>
2026-01-19 15:52:01 +0100

    main menu and game over scenes implemented

---
+++ final-project-actg/scripts/main_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@onready var start_button = $CenterContainer/VBoxContainer/Start
+@onready var instr_button = $CenterContainer/VBoxContainer/Instructions
+@onready var quit_button = $CenterContainer/VBoxContainer/Quit
+@onready var center_container = $CenterContainer
+@onready var instructions_panel = $InstructionsPanel
+
+func _ready():
+	center_container.visible = true
+	instructions_panel.visible = false
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/test_level.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _on_instructions_pressed() -> void:
+	center_container.visible = false
+	instructions_panel.visible = true
+
+func _on_play_anyway_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/test_level.tscn")
